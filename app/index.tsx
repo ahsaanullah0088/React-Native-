74b4a8468decf579ca7e8aafb9ac994d5e72b6ab
Ahsaan Ullah <ahsaanu70@gmail.com>
2025-12-21 17:26:27 +0500

    project setup done

---
+++ app/index.tsx
@@ -0,0 +1,9 @@
+import { Text, View } from "react-native";
+import './globals.css';
+export default function Index() {
+  return (
+    <View className="flex-1 justify-center items-center text-red-600">
+      <Text>Hello Ahsaan Ullah</Text>
+    </View>
+  );
+}
