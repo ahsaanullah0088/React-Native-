a5c6b3ef6bf0961c72df16ae07bebb97879e4242
Ahsaan Ullah <ahsaanu70@gmail.com>
2026-02-05 12:02:23 +0500

    again native wind installation

--- Ecommerce/app/index.tsx
+++
@@ -1,8 +0,0 @@
-export default function Index() {
-  return (
-    <div className="bg-slate-100 rounded-xl ">
-      <p className="text-lg font-medium">Welcome to Tailwind</p>
-      <p className="text-red-500"> Hello kiya hal hai</p>
-    </div>
-  );
-}
